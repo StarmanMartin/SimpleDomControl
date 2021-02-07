64bfba7c24891ec86ada6621a64b5ba4f89eed2c
StarmanMartin <m.starman@live.com>
2021-02-07 22:25:53 +0100

    fix

--- sdc_manager/management/commands/init_add/sdc_core_manager.py
+++ sdc_manager/management/commands/init_add/sdc_core_manager.py
@@ -30,7 +30,7 @@ def add_sdc_to_main_urls(main_urls_path):
 
     for line in fin:
         if 'from django.urls import path' in line:
-            line = re.sub(r'path', 'path, include', line)
+            line = re.sub(r'path', 'path, include\nfrom django.conf.urls import url', line)
 
         if "urlpatterns = [" in line:
             line = re.sub(r'urlpatterns = \[',
