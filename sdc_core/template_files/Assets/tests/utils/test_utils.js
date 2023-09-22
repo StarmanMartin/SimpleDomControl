e0ee4c8d534b015bf73ed50a2cd9996e68a4736a
Martin Starman <m.starman@live.com>
2023-09-22 10:20:30 +0200

    fixes for v0.3.2

--- sdc_core/template_files/Assets/tests/utils/test_utils.js
+++ sdc_core/template_files/Assets/tests/utils/test_utils.js
@@ -34,7 +34,7 @@ export async function get_controller(app_name, tag_name, content_html, origen_ht
     let controller, parent = new AbstractSDC();
 
     let $c = $(document.createElement(tag_name));
-    if (init_container) $c.append(init_container);
+    if (origen_html) $c.append(origen_html);
     $('body').append($c);
     const controller_flie_name = tag_name.replace(/[-]/, '_')
     await import(`../../src/${app_name}/controller/${controller_flie_name}/${controller_flie_name}.js`);
