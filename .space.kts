8ac804e24b419d94336b6eb1ceca3a00a573b862
Martin Starman <m.starman@live.com>
2024-01-10 15:27:29 +0100

    Fix kts

--- .space.kts
+++ .space.kts
@@ -17,3 +17,19 @@ job("Prepare Docker image") {
         }
     }
 }
+
+job("Run tests, build, publish") {
+    container(image = "sdc.registry.jetbrains.space/p/main/sdc-python/myimage:0.0.1") {
+        // specify URL of the package index using env var
+        env["URL"] = "https://packages.jetbrains.space/pypi/p/my-python-project/mypypi/legacy"
+
+        // We suppose that your project has default build configuration -
+        // the built package is saved to the ./dist directory
+        shellScript {
+            content = """
+                echo Run tests...
+                pytest
+            """
+        }
+    }
+}
