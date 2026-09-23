bd1411bba643439f7c41ae837d018ebc56a17ba8
Martin Starman <martin.starman@kit.edu>
2026-09-23 14:37:21 +0200

    version 0.158.9 fixed tests

--- pytests/expections/test_init/test_django_project/Assets/webpack.config/webpack.default.config.jsx
+++ pytests/expections/test_init/test_django_project/Assets/webpack.config/webpack.default.config.jsx
@@ -30,26 +30,6 @@ module.exports = (filepaths) => {
           test: /\.js$/,
           exclude: /node_modules/,
           use: ['babel-loader']
-        },
-        {
-          test: /\.s[ac]ss$/i,
-          use: [{
-            loader: 'style-loader', // inject CSS to page
-          }, {
-            loader: 'css-loader', // translates CSS into CommonJS modules
-          }, {
-            loader: 'postcss-loader', // Run post css actions
-            options: {
-              plugins: function () { // post css plugins, can be exported to postcss.config.js
-                return [
-                  require('precss'),
-                  require('autoprefixer')
-                ];
-              }
-            }
-          }, {
-            loader: 'sass-loader' // compiles Sass to CSS
-          }]
         }]
     }
   }
